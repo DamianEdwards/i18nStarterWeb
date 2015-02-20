cbe00f9eb80eb8cce2b99c3a17b6214ee321444c
damianedwards <damian@damianedwards.com>
2015-02-20 10:46:00 -0800

    Slight refactoring

--- src/Microsoft.AspNet.Localization/RequestLocalizationMiddleware.cs
+++ src/Microsoft.AspNet.Localization/RequestLocalizationMiddleware.cs
@@ -30,27 +30,38 @@ namespace Microsoft.AspNet.Localization
                     var originalCulture = CultureInfo.CurrentCulture;
                     var originalUICulture = CultureInfo.CurrentUICulture;
 
-#if ASPNETCORE50
-                    CultureInfo.CurrentCulture = culture;
-                    CultureInfo.CurrentUICulture = culture;
-#else
-                    Thread.CurrentThread.CurrentCulture = culture;
-                    Thread.CurrentThread.CurrentUICulture = culture;
-#endif
+                    SetCurrentCulture(culture, culture);
 
                     await _next(context);
 
-#if ASPNETCORE50
-                    CultureInfo.CurrentCulture = originalCulture;
-                    CultureInfo.CurrentUICulture = originalUICulture;
-#else
-                    Thread.CurrentThread.CurrentCulture = originalCulture;
-                    Thread.CurrentThread.CurrentUICulture = originalUICulture;
-#endif
+                    // NOTE: If we're on a different thread now the culture is not here but is still on the old thread
+                    //       which could be used on another request. This will be fixed by the XRE.
+
+                    SetCurrentCulture(originalCulture, originalUICulture);
 
                     return;
                 }
             }
+            else
+            {
+                // Forcibly set thread to en-US as sometimes previous threads have wrong culture across async calls, 
+                // see note above.
+                var defaultCulture = new CultureInfo("en-US");
+                SetCurrentCulture(defaultCulture, defaultCulture);
+            }
+
+            await _next(context);
+        }
+
+        private void SetCurrentCulture(CultureInfo culture, CultureInfo uiCulture)
+        {
+#if ASPNETCORE50
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = uiCulture;
+#else
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = uiCulture;
+#endif
         }
     }
 }
